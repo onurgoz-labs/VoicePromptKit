d1722d40b41f8dc68eaa435f8e733f5ac6366171
Onur GOZ <onur@maviucak.com>
2026-05-14 23:52:45 +0300

    chore: scaffold node/ts project with vitest and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  env: { node: true, es2022: true },
+  ignorePatterns: ['dist', 'node_modules', 'notes', '.notes'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+};
